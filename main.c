dd7a2f19bf7ca7a3a2950839b59d769dae587382
xanderwhitby <xwhitby20@gmail.com>
2025-04-29 13:57:27 -0600

    added tcp client to repo

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+#include <regex.h>
+#include <strings.h>
+#include <sys/socket.h>
+
+
 
 int main(){
 
---
+++ tcp_client.c
@@ -0,0 +1,12 @@
+#include <arpa/inet.h> // inet_addr()
+#include <netdb.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h> // bzero()
+#include <sys/socket.h>
+#include <unistd.h> // read(), write(), close()
+#define MAX 80
+#define PORT 8080
+#define SA struct sockaddr
+
